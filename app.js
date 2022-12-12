d6518cef1a04923168ad2d7ebfd68c123f2840b3
Ivan <ivan.halkov@gmail.com>
2022-12-12 22:41:07 +0200

    adding score
    score functionality

--- app.js
+++ app.js
@@ -3,23 +3,24 @@ window.onload = function () {
     const container = document.querySelector('.container');
     createDivElements(container);
 
-    console.log(this);
     const tiles = Array.from(document.querySelectorAll('.tile'));
     const playerDisplay = document.querySelector('.display-player');
     const resetButton = document.querySelector('#reset');
     const announcer = document.querySelector('.announcer');
-
+    
     // game control
     let board = [
         '', '', '', '', '', '', '', '', ''
     ];
-
+    
     let currentPlayer = 'X'; // is default for beginning
     let isGameActive = true; // till win or tie
 
-    console.log('stana');
     // how many are on the board
     let countMoves = 0;
+    let xWon = 0;
+    let oWon = 0;
+    let tie = 0;
 
     // addEventListener on the container
     container.addEventListener('click', onTileClick);
@@ -58,7 +59,6 @@ window.onload = function () {
     resetButton.addEventListener('click', onReset);
 
     function onReset() {
-        // announcer.classList.add('hide');
         announcer.textContent = '';
         currentPlayer = 'X';
         isGameActive = true;
@@ -83,6 +83,86 @@ window.onload = function () {
         playerDisplay.textContent = 'X';
         playerDisplay.classList.remove('playerO');
         playerDisplay.classList.add('playerX');
+    }
+
+    function checkForWin(board, announcer, currentPlayer) {
+        let isWin = false;
+        let winningPlayer = '';
+    
+    
+        // end states to avoid typos
+        const endStates = {
+            'x': 'Player X Won',
+            'o': 'Player O Won',
+            'tie': 'Tie'
+        };
+    
+        const winningConditions = [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 4, 6],
+            [2, 5, 8],
+            [3, 4, 5],
+            [6, 7, 8],
+        ];
+            
+        for (let r = 0; r < winningConditions.length; r++) {
+            let currRow = winningConditions[r]; // is arr
+            let prevInd = currRow[0]; // is initialized the 1st value of the arr
+            let prevVal = board[prevInd];
+    
+            if (prevVal == '') {
+                continue;
+            }
+    
+            let same = 1;
+            
+            for (let c = 1; c < currRow.length; c++) {
+                let currInd = currRow[c];
+                let currVal = board[currInd];
+    
+                if (currVal != prevVal) {
+                    break;
+                }
+    
+                same += 1;
+            }
+    
+            if (same === 3) {
+                // there is win
+                isWin = true;
+                winningPlayer = prevVal;
+                if (winningPlayer == 'X') {
+                    xWon += 1;
+                } else {
+                    oWon += 1;
+                }
+                break;
+            }
+            
+        }
+    
+        if (isWin) {
+            let key = winningPlayer.toLowerCase();
+            let winner = document.getElementById(`${key}Won`);
+            
+            if (key == 'x') {
+                winner.textContent = `X = ${xWon}`;
+            } else {
+                winner.textContent = `O = ${oWon}`;
+            }
+
+            return false;
+        } else if (!board.includes('')) {
+            tie += 1;
+            document.getElementById('tie').textContent = `TIE = ${tie}`;
+            return false;
+        } else {
+            return true;
+        }
+    
     }
 }
 
@@ -119,10 +199,9 @@ function changePlayer(playerDisplay, currentPlayer) {
 }
 
 function announce(announcer, endResult, currentPlayer) {
-    // announcer is in a variable because is used more than one time
-    // some colors can be added with relevant span element
-    announcer.textContent = `${endResult}`;
-    announcer.classList.remove('hide');
+    // announcer.textContent = `${endResult}`;
+    // announcer.classList.remove('hide');
+
     if (endResult == 'Tie') {
         announcer.classList.add('tie');
     } else {
@@ -130,70 +209,3 @@ function announce(announcer, endResult, currentPlayer) {
     }
 }
 
-function checkForWin(board, announcer, currentPlayer) {
-    let isWin = false;
-    let winningPlayer = '';
-
-    // end states to avoid typos
-    const endStates = {
-        'x': 'Player X Won',
-        'o': 'Player O Won',
-        'tie': 'Tie'
-    };
-
-    const winningConditions = [
-        [0, 1, 2],
-        [0, 3, 6],
-        [0, 4, 8],
-        [1, 4, 7],
-        [2, 4, 6],
-        [2, 5, 8],
-        [3, 4, 5],
-        [6, 7, 8],
-    ];
-        
-    for (let r = 0; r < winningConditions.length; r++) {
-        let currRow = winningConditions[r]; // is arr
-        let prevInd = currRow[0]; // is initialized the 1st value of the arr
-        let prevVal = board[prevInd];
-
-        if (prevVal == '') {
-            continue;
-        }
-
-        let same = 1;
-        
-        for (let c = 1; c < currRow.length; c++) {
-            let currInd = currRow[c];
-            let currVal = board[currInd];
-
-            if (currVal != prevVal) {
-                break;
-            }
-
-            same += 1;
-        }
-
-        if (same === 3) {
-            // there is win
-            isWin = true;
-            winningPlayer = prevVal;
-            break;
-        }
-        
-    }
-
-    if (isWin) {
-        let key = winningPlayer.toLowerCase();
-        // console.log(endStates[key]);
-        // announce(endStates[key]);
-        announce(announcer, endStates[key], currentPlayer)
-        return false;
-    } else if (!board.includes('')) {
-        announce(announcer, endStates['tie'], currentPlayer)
-        return false;
-    } else {
-        return true;
-    }
-
-}
